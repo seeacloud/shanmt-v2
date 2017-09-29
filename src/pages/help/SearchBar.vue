0e1cf88725979be00ed964091feaa22dcaddeec6
seeacloud <seeacloud@gmail.com>
2017-09-29 17:27:03 +0800

    v2

--- src/pages/help/SearchBar.vue
+++ src/pages/help/SearchBar.vue
@@ -1,41 +1,46 @@
 <template>
-    <div style="background:#fff;">
-      <div class="con">
-        <div class="bar">
-          <input type="text" placeholder="更多问题请输入问题关键字"><Button type="primary" icon="ios-search" class="btn">搜索</Button>
-        </div>
+  <div style="background:#ffffff;">
+    <div class="con">
+      <div class="bar">
+        <input type="text" placeholder="更多问题请输入问题关键字">
+        <Button type="primary" icon="ios-search" class="btn" @click="gotohelp">搜索</Button>
       </div>
-
     </div>
+
+  </div>
 </template>
 
 <script>
   export default {
     data () {
       return {}
+    },
+    methods: {
+      gotohelp: function () {
+        window.location.assign('http://shanmt.com/admin/help/doccenter')
+      }
     }
   }
 </script>
 
 <style lang='scss' scoped>
   @import '../../../static/common';
-  .con
-  {
+
+  .con {
   }
-  .bar
-  {
+
+  .bar {
     position: relative;
     width: 800px;
     margin: -30px auto 0 auto;
     display: block;
-    background: #fff;
+    background: #ffffff;
     z-index: 100;
     height: 60px;
     -webkit-box-shadow: 2px 2px 10px #aaaaaa;
     -moz-box-shadow: 2px 2px 10px #aaaaaa;
     box-shadow: 2px 2px 10px #aaaaaa;
-    input[type="text"]
-    {
+    input[type="text"] {
       border: none;
       background: none;
       width: 700px;
@@ -43,18 +48,16 @@
       line-height: 60px;
       font-size: 16px;
     }
-    input:focus
-    {
+    input:focus {
       outline: none;
     }
   }
-  .btn
-  {
+
+  .btn {
     border-radius: 0;
     height: 60px;
     float: right;
     font-size: 16px;
     height: 60px;
-
   }
 </style>
--- src/pages/products/SmtOrder.vue
+++ src/pages/products/SmtOrder.vue
@@ -36,7 +36,7 @@
           subtitle: '智能、高效的订货管理系统，全面提升订货效率',
           codeurl: './static/img/app.jpg',
           btn: '立即购买',
-          bgimg: 'url(' + require('../../../static/img/macair-banner.jpg') + ')' + 'no-repeat center',
+          bgimg: 'url(' + require('../../../static/img/smt-order/imac-o-banner.jpg') + ')' + 'no-repeat center',
           mainimg: '',
           fontcolor: 'blue'
         },
@@ -45,10 +45,10 @@
           subtitle: '电脑手机全支持，指尖上办公，让各环节工作从此化繁为简，是您可靠的智能伙伴',
           captitle: '先进的订货管理系统',
           captions: [
-            {des: '山木通云订货平台可一键完成销售开单,厂家订货,即时生成应收款、应付款、毛利润等经营报表,库存智能管理,云端存储数据，十八般武艺，样样精通'}
+            {des: '山木通云订货平台可一键完成销售开单,订货,即时生成应收款、应付款、毛利润等经营报表,库存智能管理,云端存储数据，十八般武艺，样样精通'}
           ],
           btn: '',
-          imgurl: './static/img/laptopiphone.jpg'
+          imgurl: './static/img/smt-order/laptopiphone-o.jpg'
         },
         secinfo2: {
           title: '',
@@ -57,10 +57,12 @@
           captions: [
 //            {des: '● 订单管理、生产管理、库存管理让企业生产经营井井有条'},
 //            {des: '● 资金管理、报表系统让您对企业运筹帷幄'},
-            {des: '山木通门店版对库存管理进行了升级, 门店可以录入自有产品,实现灵活的库存管理。门店开业自由录入产品,也可以实时查看库存状态'}
+            {des: '山木通云订货平台对库存管理进行了升级,实现灵活的库存管理。智能高效的完成繁琐的库存管理,让库存管理更轻松'}
           ],
           btn: '',
-          imgurl: './static/img/laptop-order-2.jpg'
+//          imgurl: './static/img/laptop-order-2.jpg'
+          imgurl: './static/img/smt-order/laptopiphone-o-stock.jpg'
+
         },
         secinfo3: {
           title: '',
@@ -70,30 +72,30 @@
             {des: '即使线下资金往来, 也可以方便的录入系统, 让您的账目井井有条, 拒绝糊涂账, 财务状况一目了然'}
           ],
           btn: '',
-          imgurl: './static/img/laptop-money.png'
+          imgurl: './static/img/smt-order/laptopiphone-o-money.jpg'
         },
         secinfo4: {
           title: '',
           subtitle: '',
-          captitle: '订单状态随时记录',
+          captitle: '实用的报表系统',
           captions: [
 //            {des: '● 采用最新SaaS技术，无需安装维护，节省维护成本'},
 //            {des: '● 操作简单，一用就会，上手快'},
             {des: '山木通云管理平台门店版可以手动录入订单状态, 让您可以方便的更新和记录订单状态,订单状态,您心里有数!'}
           ],
           btn: '',
-          imgurl: './static/img/iphone-half/iphone-order.jpg'
+          imgurl: './static/img/smt-order/laptopiphone-o-report.jpg'
         },
         cards: [
           {title: '产品管理', des: '直观的产品展示，电脑、手机的应用，随时随地和客户分享', iconcode: '856;'},
-          {title: '订货管理', des: '强大的订单管理中心，全流程的订单跟踪展示，让客户、厂家及时了解订单情况', iconcode: '608;'},
+          {title: '订货管理', des: '强大的订单管理中心，全流程的订单跟踪展示，让您及时了解订单情况', iconcode: '608;'},
           {title: '库存管理', des: '通过库存管理功能进行产品的入库、出库、盘点、库存状况查询', iconcode: '613;'},
 //          {title: '生产管理', des: '生产全流程监控，让客户及时掌握订单生产状态，做到心中有数', iconcode: '662;'},
           {title: '资金管理', des: '实时掌握客户资金账户情况，严格控制资金风险，及时督促财务收款', iconcode: '626;'},
-          {title: '报表中心', des: '强大的分析报表功能，实现厂家的订货、生产、资金、库存的全方位分析', iconcode: '7d5;'},
-          {title: '营销', des: '门店账户设置，供应商管理，默认地址配置等', iconcode: '614;'},
-          {title: '移动办公', des: '移动化办公(电脑、手机全支持)，老板不在公司也能随时了解公司经营情况', iconcode: '640;'},
-          {title: '微信跟踪', des: '关注山木通微信公众号，就能了解到产品生产状态，通过平台进行产品的真伪验证', iconcode: '65a;'}
+          {title: '报表中心', des: '强大的分析报表功能，实现订货、资金、库存等方面的全方位分析', iconcode: '7d5;'},
+          {title: '营销', des: '多种多样的营销策略，让产品火爆热卖', iconcode: '614;'},
+          {title: '移动办公', des: '移动化办公(电脑、手机全支持)，不在公司也能随时了解公司经营情况', iconcode: '640;'},
+          {title: '微信跟踪', des: '关注山木通微信公众号，就能随时了解到订单状态，方便快捷', iconcode: '65a;'}
         ]
       }
     },
